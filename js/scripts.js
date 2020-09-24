2b907676797778f5fe6ab1c9c8a9307219ad7c71
Irimurielle <irimurielle@gmail.com>
2020-09-24 10:59:09 +0200

    addedthe conversion from celsius to fahrenheit

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,9 @@ var add = function(number1, number2) {
     var height = parseFloat(prompt("Enter your height in cm"));
     var result = BMI (mass, height);
     alert("Your body mass index is :" + " " + result);
+    var temperature = function (celsius){
+      return (celsius * (9 / 5)) + (32);
+      }
+      var celsius = parseFloat(prompt("Enter the temperature in celsius"));
+      var conversion = temperature(celsius);
+      alert("Here is the temperature in fahrenheit :" + " " + conversion);
